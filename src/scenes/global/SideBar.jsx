267f1445999bf40e49fea87db36101a5bd7236f9
apkap83 <33512036+apkap83@users.noreply.github.com>
2022-10-18 09:15:58 +0300

    First Commit

---
+++ src/scenes/global/SideBar.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { ProSideBar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+
+import HomeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+const SideBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollaped, setIsCollaped] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return <></>;
+};
+
+export default SideBar;
